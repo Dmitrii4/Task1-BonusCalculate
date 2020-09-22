b3abebac132e847ad1150280a81d18dd40d3abe0
dmitrii <dmitrii.churbanov@gmail.com>
2020-09-22 18:49:08 +0300

    v1

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        // если true - то PASS
+        // если false - то FAIL
+        boolean passed = expected == actual;
+
+        // выводим результат
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        // если true - то PASS
+        // если false - то FAIL
+        boolean passed = expected == actual;
+
+        // выводим результат
+        assertEquals(expected,actual);
+    }
+}
